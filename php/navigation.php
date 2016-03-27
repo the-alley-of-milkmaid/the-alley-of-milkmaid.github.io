e85f1bb230f022d338e36aa70d6ed595f9bb25b4
Mohamed El sherif <mohamedalsherif@hotmail.com>
2016-03-27 18:59:45 +0200

    Working Version of Website Without share Button

--- php/navigation.php
+++ php/navigation.php
@@ -3,7 +3,7 @@
 <!-- Nav tabs -->
 <ul class="nav nav-tabs removeBorderBottom" role="tablist">
 <li id="theNav0" class="active">  <a href="#" onClick="goto(0, this); return false">ABOUT</a></li>
-<li id="theNav1">  <a href="#" onClick="goto(1, this); return false">ARTWORK</a></li>
+<li id="theNav1">  <a href="#" onClick="goto(1, this); return false">ART</a></li>
 <li id="theNav2">  <a href="#" onClick="goto(2, this); return false">EXHIBITIONS</a></li>
 <li id="theNav3">  <a href="#" onClick="goto(3, this); return false">CONTACT</a></li>
 </ul>

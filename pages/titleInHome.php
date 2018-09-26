47cb61afe5ec44db4656f8b1c090247d52f74d3e
Mohamed El Sherif <mohamed.elsherif@100worte.de>
2018-09-26 13:03:39 +0200

    ädded migo stuff

--- pages/titleInHome.php
+++
@@ -1,6 +0,0 @@
-<div id="divOfName"  class="visib_hidden shape-with-text">
-
-	<img src="public/img/other/LogoFront.png" class="nourineName"  >
-		<!--<p id="titlePElement" style="padding-top: 0pt; " class="paragraph_style"><span class="style_1 nourineName"> NOURINE</span><span class="nourineName style_2">HAMMAD</span><br /></p>
->-->
-</div>
